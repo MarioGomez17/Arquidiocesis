19dbb704af8df3a914130af41dd5ab9538ea4348
MarioGomez17 <mg441236@gmail.com>
2025-04-29 19:01:48 -0500

    ADD MAIN LAYOUT TO APP. The main layout was added in the APP component to use it in all the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'arquidiocesis';
