04d410945e54705f4051336aaf12f9429b223864
MarioGomez17 <mg441236@gmail.com>
2025-05-05 18:43:49 -0500

    UPDATED HOME VIEW Home view was updated to show all the archdiocese information

--- src/app/views/home-view/home-view.component.ts
+++ src/app/views/home-view/home-view.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FunctionalitiesContainerComponent } from "../../features/home/functionalities/functionalities-container/functionalities-container.component";
+import { ArchdioceseContainerComponent } from "../../features/home/archdiocese/archdiocese-container/archdiocese-container.component";
+import { ProceduresContainerComponent } from "../../features/home/procedures/procedures-container/procedures-container.component";
+import { CalendarContainerComponent } from "../../features/home/calendar-events/calendar-container/calendar-container.component";
+import { NewsEventContainerComponent } from "../../features/home/news-and-events/news-event-container/news-event-container.component";
+import { ContactContainerComponent } from "../../features/home/contact/contact-container/contact-container.component";
+import { OfficeRadioContainerComponent } from "../../features/home/office-and-radio/office-radio-container/office-radio-container.component";
 
 @Component({
   selector: 'app-home-view',
-  imports: [],
+  imports: [FunctionalitiesContainerComponent, ArchdioceseContainerComponent, ProceduresContainerComponent, CalendarContainerComponent, NewsEventContainerComponent, ContactContainerComponent, OfficeRadioContainerComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css',
 })
