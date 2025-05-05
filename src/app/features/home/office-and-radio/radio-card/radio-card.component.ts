97d778038432b7016cb7d589ddf264c43ba9cfe2
MarioGomez17 <mg441236@gmail.com>
2025-05-05 18:42:03 -0500

    ADD RADIO COMPONENTS All the components to show radio info was added to the Project

---
+++ src/app/features/home/office-and-radio/radio-card/radio-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-radio-card',
+  imports: [],
+  templateUrl: './radio-card.component.html',
+  styleUrl: './radio-card.component.css',
+})
+export class RadioCardComponent {}
